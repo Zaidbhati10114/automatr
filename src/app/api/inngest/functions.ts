df82e63420a4bcd997dff06095034d931a9604ff
Zaidbhati10114 <zaidbhati007@gmail.com>
2025-10-30 15:59:00 +0530

    ai-providers-added

--- src/app/api/inngest/functions.ts
+++ src/app/api/inngest/functions.ts
@@ -1,11 +1,25 @@
 import { inngest } from "@/inngest/client";
+import { createGoogleGenerativeAI } from '@ai-sdk/google'
+import { generateText } from 'ai'
+const google = createGoogleGenerativeAI()
 
 
-export const helloWorld = inngest.createFunction(
-    { id: "hello-world" },
-    { event: "test/hello.world" },
+export const execute = inngest.createFunction(
+    { id: "execute-ai" },
+    { event: "execute/ai" },
     async ({ event, step }) => {
-        await step.sleep("wait-a-moment", "10s");
-        return { message: `Hello ${event.data.email}!` };
+        const { steps } = await step.ai.wrap(
+            "gemini-generate-text",
+            generateText,
+            {
+                model: google('gemini-2.5-flash'),
+                system: "You are a helpful assistant.",
+                prompt: "What is 2 + 2 ?"
+            }
+
+        )
+
+        return steps
+
     },
 );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,17 @@
 
 import { inngest } from '@/inngest/client';
-import { createTRPCRouter, protectedProcedure } from '../init';
+import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
+import { google } from '@ai-sdk/google';
+import { generateText } from 'ai';
 
 export const appRouter = createTRPCRouter({
+    testAi: baseProcedure.mutation(async () => {
+        await inngest.send({
+            name: 'execute/ai',
+        });
+        return { success: true, message: 'Job Queued' }
+    }),
     getWorkflows: protectedProcedure.query(({ ctx }) => {
         return prisma.workflow.findMany();
     }),
