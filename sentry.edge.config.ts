283bfbd929c7b3e62f4879f4fe82a93aa33a5c64
Zaidbhati10114 <zaidbhati007@gmail.com>
2025-11-03 19:46:22 +0530

    sentry-added

---
+++ sentry.edge.config.ts
@@ -0,0 +1,22 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://62bfb35479080d4e258df1d79d91b7b4@o4510261722021888.ingest.us.sentry.io/4510294931210240",
+  integrations: [
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "warn", "error"] })
+  ],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
--- src/app/api/inngest/functions.ts
+++ src/app/api/inngest/functions.ts
@@ -1,4 +1,5 @@
 import { inngest } from "@/inngest/client";
+import * as Sentry from "@sentry/nextjs";
 import { createGoogleGenerativeAI } from '@ai-sdk/google'
 import { generateText } from 'ai'
 const google = createGoogleGenerativeAI()
@@ -8,13 +9,22 @@ export const execute = inngest.createFunction(
     { id: "execute-ai" },
     { event: "execute/ai" },
     async ({ event, step }) => {
+        await step.sleep("pretend", "5s")
+        Sentry.logger.info('user triggedred test log', { log_source: 'sentry_test' })
+        console.warn('Something is missing')
+        console.error('This is an sample error')
         const { steps } = await step.ai.wrap(
             "gemini-generate-text",
             generateText,
             {
                 model: google('gemini-2.5-flash'),
                 system: "You are a helpful assistant.",
-                prompt: "What is 2 + 2 ?"
+                prompt: "What is 2 + 2 ?",
+                experimental_telemetry: {
+                    isEnabled: true,
+                    recordInputs: true,
+                    recordOutputs: true,
+                }
             }
 
         )
